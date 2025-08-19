3b6be9970183c421a141b77dc3bfa3c39904eb01
Koliya03 <senarathnakoliya@gmail.com>
2025-08-19 12:36:02 +0530

    Created the Rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle implements Shape {
+    private final double width, height;
+    private final double thickness;
+
+    public Rectangle(double width, double height, double thickness) {
+        if (width <= 0 || height <= 0)
+            throw new IllegalArgumentException("Width or height can only be an integer");
+        if (thickness < 0)
+            throw new IllegalArgumentException("thickness >= 0");
+        this.width = width;
+        this.height = height;
+        this.thickness = thickness;
+    }
+
+    @Override
+    public double areaWithoutBorder() {
+        return width * height;
+    }
+
+    @Override
+    public double areaIncludingBorder() {
+        return (width + (2 * thickness)) * (height + (2 * thickness));
+    }
+
+    @Override
+    public double borderArea() {
+        return areaIncludingBorder() - areaWithoutBorder();
+    }
+}
